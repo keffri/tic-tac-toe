9724b5abdeb04bb85705f836912362b0b12f5d58
keffri <keffri.w.neal@gmail.com>
2020-11-29 13:41:20 -0800

    basic function

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
-// let player = (() => {
-//   let playerName = prompt("Please enter your name.");
-//   if (playerName === null) {
-//     alert("Please enter a name.");
-//   }
-//   return { playerName };
-// })();
+const createPlayer = () => {
+  playerName = prompt("Please enter a name.");
+  while (playerName === null || undefined) {
+    playerName = prompt("Please enter a name!");
+  }
+  name = playerName;
+  return { name };
+};
 
-const displayController = (() => {
-  let gameBoard = document.querySelector(".ticTacToeBoard");
+let playerOne = createPlayer();
+let playerTwo = createPlayer();
 
-  let createBoard = (() => {
-    for (let i = 1; i <= 9; i++) {
-      let boardBox = document.createElement("div");
-      boardBox.classList.add("boardBox");
-      gameBoard.appendChild(boardBox);
-    }
-  })();
+const gameLogic = (() => {
+  let board;
+  let boxes = Array.from(document.querySelectorAll(".boardBox"));
+  let turn;
+  let playersTurn;
+  let win;
+  let turnText = document.getElementById("turnText");
+  document
+    .getElementById("ticTacToeBoard")
+    .addEventListener("click", handleClick);
 
-  return { gameBoard };
-})();
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-const gameBoard = (() => {
-  let gameBoard = document.querySelector(".ticTacToeBoard");
-  let boardBoxes = document.querySelectorAll(".boardBox");
-  let board = ["", "", "", "", "", "", "", "", ""];
+  function boardInit() {
+    board = ["", "", "", "", "", "", "", "", ""];
+    turn = "X";
+    playersTurn = playerOne;
+    turnText.textContent = `It's ${playersTurn.name}'s turn. (${turn})`;
+    createMark();
+  }
 
-  boardBoxes.forEach((box) => {
-    box.addEventListener("click", () => {
-      console.log("test");
+  function createMark() {
+    board.forEach((mark, index) => {
+      boxes[index].textContent = mark;
     });
-  });
+  }
 
-  return { boardBoxes };
+  function handleClick(e) {
+    let clickedBox = e.target;
+    if (clickedBox.textContent !== "") {
+      return;
+    }
+    clickedBox.textContent = turn === "X" ? "X" : "O";
+    switchTurn();
+    let win = checkWinner();
+    turnText.textContent = `It's ${playersTurn.name}'s turn. (${turn}) `;
+  }
+
+  function switchTurn() {
+    turn = turn === "X" ? "O" : "X";
+    playersTurn = playersTurn === playerOne ? playerTwo : playerOne;
+  }
+
+  function checkWinner() {
+    let winner = null;
+
+    return winner ? winner : board.includes("") ? null : "T";
+  }
+
+  boardInit();
+  return { win };
 })();
